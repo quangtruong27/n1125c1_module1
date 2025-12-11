9fc67f05c69c17a1f63c958fe69c8b486f6dff07
quangtruong27 <120413460+quangtruong27@users.noreply.github.com>
2025-12-11 19:44:35 +0700

    ss9

--- src/ss9_array/Bt03.java
+++ src/ss9_array/Bt03.java
@@ -2,33 +2,17 @@ package ss9_array;
 
 import java.util.Scanner;
 
-/**
- * Dựa trên chương trình quản lý nhân viên ở bài trước, thực hiện các yêu cầu sau:
- * <p>
- * a. Tìm nhân viên có điểm trung bình cao nhất.
- * <p>
- * b. Tìm nhân viên theo họ tên (tìm chính xác).
- * <p>
- * c. Tìm nhân viên theo họ tên (tìm mềm - Gần chính xác)
- * <p>
- * Ví dụ: Khi nhập một phần của họ tên (ví dụ: "Hoàng"),
- * hệ thống sẽ xuất ra tất cả các nhân viên có chữ "Hoàng" xuất hiện trong họ tên của họ (ví dụ: "Nguyễn Văn Hoàng",
- * "Hoàng Văn Thái", ...).
- */
-
 public class Bt03 {
+
 	static Scanner sc = new Scanner(System.in);
+	static String[] hoTen;
+	static int[] tuoi;
+	static String[] gioiTinh;
+	static double[] luong;
+	static double[] diemTB;
 
-	public static void inputEmployee(
-			String[] hoTen,
-			int[] tuoi,
-			String[] gioiTinh,
-			double[] luong,
-			double[] diemTB,
-			int index
-
-	) {
-		System.out.println("\n Nhap nhan vien thu " + (index + 1));
+	public static void inputEmployee(int index) {
+		System.out.println("\nNhap nhan vien thu " + (index + 1));
 
 		System.out.print("Ho ten: ");
 		hoTen[index] = sc.nextLine();
@@ -49,14 +33,7 @@ public class Bt03 {
 		sc.nextLine();
 	}
 
-	public static void outputEmployee(
-			String[] hoTen,
-			int[] tuoi,
-			String[] gioiTinh,
-			double[] luong,
-			double[] diemTB,
-			int index
-	) {
+	public static void outputEmployee(int index) {
 		System.out.println("\nNhan vien thu " + (index + 1));
 		System.out.println("Ho ten: " + hoTen[index]);
 		System.out.println("Tuoi: " + tuoi[index]);
@@ -65,14 +42,8 @@ public class Bt03 {
 		System.out.println("Diem TB: " + diemTB[index]);
 	}
 
-	// a. Tìm nhân viên có điểm trung bình cao nhất.
-	public static void findMaxDiemTB(
-			String[] hoTen,
-			int[] tuoi,
-			String[] gioiTinh,
-			double[] luong,
-			double[] diemTB
-	) {
+
+	public static void findMaxDiemTB() {
 		double max = diemTB[0];
 
 		for (double d : diemTB) {
@@ -84,26 +55,19 @@ public class Bt03 {
 		System.out.println("\nNhan vien co diem TB cao nhat: " + max);
 		for (int i = 0; i < diemTB.length; i++) {
 			if (diemTB[i] == max) {
-				outputEmployee(hoTen, tuoi, gioiTinh, luong, diemTB, i);
+				outputEmployee(i);
 			}
 		}
 	}
 
-	// b. Tìm nhân viên theo họ tên (tìm chính xác).
-	public static void findByNameExact(
-			String[] hoTen,
-			int[] tuoi,
-			String[] gioiTinh,
-			double[] luong,
-			double[] diemTB,
-			String name
-	) {
+
+	public static void findByNameExact(String name) {
 		boolean tim = false;
 
 		System.out.println("\nKet qua tim kiem (chinh xac)");
 		for (int i = 0; i < hoTen.length; i++) {
 			if (hoTen[i].equalsIgnoreCase(name)) {
-				outputEmployee(hoTen, tuoi, gioiTinh, luong, diemTB, i);
+				outputEmployee(i);
 				tim = true;
 			}
 		}
@@ -113,21 +77,15 @@ public class Bt03 {
 		}
 	}
 
-	// c. Tìm nhân viên theo họ tên (tìm mềm - Gần chính xác)
-	public static void findByNameSoft(
-			String[] hoTen,
-			int[] tuoi,
-			String[] gioiTinh,
-			double[] luong,
-			double[] diemTB,
-			String keyword
-	) {
+
+	public static void findByNameSoft(String keyword) {
 		boolean tim = false;
+		keyword = keyword.toLowerCase();
 
 		System.out.println("\nKet qua tim kiem (gan dung)");
 		for (int i = 0; i < hoTen.length; i++) {
 			if (hoTen[i].toLowerCase().contains(keyword)) {
-				outputEmployee(hoTen, tuoi, gioiTinh, luong, diemTB, i);
+				outputEmployee(i);
 				tim = true;
 			}
 		}
@@ -144,34 +102,34 @@ public class Bt03 {
 		int n = sc.nextInt();
 		sc.nextLine();
 
-
-		String[] hoTen = new String[n];
-		int[] tuoi = new int[n];
-		String[] gioiTinh = new String[n];
-		double[] luong = new double[n];
-		double[] diemTB = new double[n];
+		hoTen = new String[n];
+		tuoi = new int[n];
+		gioiTinh = new String[n];
+		luong = new double[n];
+		diemTB = new double[n];
 
 		// Nhap
 		for (int i = 0; i < n; i++) {
-			inputEmployee(hoTen, tuoi, gioiTinh, luong, diemTB, i);
+			inputEmployee(i);
 		}
 
 		// Xuat
 		System.out.println("\nDANH SACH NHAN VIEN");
 		for (int i = 0; i < n; i++) {
-			outputEmployee(hoTen, tuoi, gioiTinh, luong, diemTB, i);
+			outputEmployee(i);
 		}
 
+		// a. Tim diem TB cao nhat
+		findMaxDiemTB();
 
-		findMaxDiemTB(hoTen, tuoi, gioiTinh, luong, diemTB);
-
-		System.out.print("\nNhap nhanv vien (chinh xac): ");
+		// b. Tim theo ten chinh xac
+		System.out.print("\nNhap ten can tim (chinh xac): ");
 		String nameExact = sc.nextLine();
-		findByNameExact(hoTen, tuoi, gioiTinh, luong, diemTB, nameExact);
+		findByNameExact(nameExact);
 
-		System.out.print("\nNhap nhan vien (gan chinh xac: ");
+		// c. Tim theo ten gan dung
+		System.out.print("\nNhap tu khoa tim kiem: ");
 		String keyword = sc.nextLine();
-		findByNameSoft(hoTen, tuoi, gioiTinh, luong, diemTB, keyword);
+		findByNameSoft(keyword);
 	}
 }
-
